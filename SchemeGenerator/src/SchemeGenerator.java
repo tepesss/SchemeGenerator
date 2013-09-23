563294c60ad48b9093822dcc562fea8d56b0d2d3
tepesss <tepesss@gmail.com>
2013-09-23 11:19:32 +0300

    1 added empty src under git control

---
+++ SchemeGenerator/src/SchemeGenerator.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Volodymyr_Kychak
+ * Date: 9/23/13
+ * Time: 10:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SchemeGenerator {
+    public static void main (String[] args){
+        System.out.print("sdfjkl");
+    }
+}
