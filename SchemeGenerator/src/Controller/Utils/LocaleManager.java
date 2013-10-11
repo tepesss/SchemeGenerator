56f24953fa5503126489d457938fa057ed8272c3
tepesss <tepesss@gmail.com>
2013-10-11 18:02:34 +0300

    added classes to the project

---
+++ SchemeGenerator/src/Controller/Utils/LocaleManager.java
@@ -0,0 +1,18 @@
+package Controller.Utils;
+
+import java.util.Locale;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Volodymyr_Kychak
+ * Date: 10/11/13
+ * Time: 3:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocaleManager {
+    static public final Locale ENGLISH = new Locale("uk", "UA");
+    static public final Locale UKRAINIAN = Locale.ENGLISH;
+    static public final Locale RUSSIAN = new Locale("ru", "RU");
+//    public void localize(Component c){
+//    }
+}
